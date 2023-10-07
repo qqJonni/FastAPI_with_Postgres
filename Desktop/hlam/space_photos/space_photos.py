ef5db48a41f9df07cc5a9e84c748caf1c00f23d6
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-07 21:46:31 +0500

    add upload_image func

--- Desktop/hlam/space_photos/space_photos.py
+++ Desktop/hlam/space_photos/space_photos.py
@@ -1,11 +1,15 @@
 import os
 import requests
 
-filename = os.path.join('images', 'hubble.jpeg')
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-response = requests.get(url)
-response.raise_for_status()
 
-with open(filename, 'wb')as file:
-    file.write(response.content)
+def upload_image(url, upload_to):
+    filename = os.path.join(upload_to, 'hubble.jpeg')
+    url = url
+    response = requests.get(url)
+    response.raise_for_status()
 
+    with open(filename, 'wb')as file:
+        file.write(response.content)
+
+
+upload_image('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'images')
