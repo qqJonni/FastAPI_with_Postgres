8dbb8c36229fb6e707a6747cd509abea902b8a60
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-07 22:32:29 +0500

    add new photo

--- Desktop/hlam/space_photos/space_photos.py
+++ Desktop/hlam/space_photos/space_photos.py
@@ -1,9 +1,10 @@
 import os
 import requests
+import json
 
 
 def upload_image(url, upload_to):
-    filename = os.path.join(upload_to, 'hubble.jpeg')
+    filename = os.path.join(upload_to, f'spacex{url_number}.jpeg')
     url = url
     response = requests.get(url)
     response.raise_for_status()
@@ -12,4 +13,13 @@ def upload_image(url, upload_to):
         file.write(response.content)
 
 
-upload_image('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'images')
+url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+response = requests.get(url)
+response.raise_for_status()
+
+data_json = json.loads(response.content)
+
+original_links = data_json["links"]["flickr"]["original"]
+
+for url_number, url in enumerate(original_links):
+    upload_image(url, 'images')
