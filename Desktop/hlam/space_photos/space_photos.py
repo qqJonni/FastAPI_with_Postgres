eb621c671b5a6b488a90f13689a9b7324e81b201
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-07 21:04:39 +0500

    add images folder

--- Desktop/hlam/space_photos/space_photos.py
+++ Desktop/hlam/space_photos/space_photos.py
@@ -1,6 +1,7 @@
+import os
 import requests
 
-filename = 'hubble.jpeg'
+filename = os.path.join('images', 'hubble.jpeg')
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 response = requests.get(url)
 response.raise_for_status()
