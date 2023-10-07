67e499c1ec21b7ceda9a45d783bb7f4e18a489ff
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-07 22:56:54 +0500

    add fetch_spacex_last_launch func

--- Desktop/hlam/space_photos/space_photos.py
+++ Desktop/hlam/space_photos/space_photos.py
@@ -4,7 +4,7 @@ import json
 
 
 def upload_image(url, upload_to):
-    filename = os.path.join(upload_to, f'spacex{url_number}.jpeg')
+    filename = os.path.join(upload_to, 'hubble.jpeg')
     url = url
     response = requests.get(url)
     response.raise_for_status()
@@ -13,13 +13,19 @@ def upload_image(url, upload_to):
         file.write(response.content)
 
 
-url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
-response = requests.get(url)
-response.raise_for_status()
+def fetch_spacex_last_launch(url, upload_to):
+    url = url
+    response = requests.get(url)
+    response.raise_for_status()
 
-data_json = json.loads(response.content)
+    data_json = json.loads(response.content)
 
-original_links = data_json["links"]["flickr"]["original"]
+    original_links = data_json["links"]["flickr"]["original"]
 
-for url_number, url in enumerate(original_links):
-    upload_image(url, 'images')
+    for url_number, url in enumerate(original_links):
+        filename = os.path.join(upload_to, f'spacex{url_number}.jpeg')
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
+
+fetch_spacex_last_launch('https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a', 'images')
