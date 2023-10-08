d444af31a38508213377b998be14bd71099ed64e
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-08 20:37:22 +0500

    publish photo to Telegram channel

--- Desktop/hlam/space_photos/telegram_bot.py
+++ Desktop/hlam/space_photos/telegram_bot.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 bot = telegram.Bot(os.environ.get('TELEGRAM_TOKEN'))
 
-bot.send_message(chat_id='1500316931', text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_photo('1500316931', open('/Users/valeriy/Downloads/photo-thumb-37546.png', 'rb'))
 
