ae0f126a4e78956d58188a8f62a9696c0d5b9831
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-08 17:14:15 +0500

    add upload_epic_nasa_photo func

--- Desktop/hlam/space_photos/space_photos.py
+++ Desktop/hlam/space_photos/space_photos.py
@@ -57,4 +57,27 @@ def upload_apod_nasa_images(url, upload_to):
             file.write(response.content)
 
 
+def upload_epic_nasa_photo(url, upload_to):
+    load_dotenv(find_dotenv())
+    token = os.environ.get('APOD_TOKEN')
+
+    url = f'{url}{token}'
+    response = requests.get(url)
+    data_json = json.loads(response.content)
+    original_dates = [entry['date'] for entry in data_json if 'date' in entry]
+    original_image = [entry['image'] for entry in data_json if 'image' in entry]
+
+    original_links = []
+    for date, image in zip(original_dates, original_image):
+        year = date[0:4]
+        month = date[5:7]
+        day = date[8:10]
+        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image}.png?api_key={token}'
+        original_links.append(image_url)
+
+    for url_number, url in enumerate(original_links):
+        filename = os.path.join(upload_to, f'epic_photo_{url_number}.png')
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
 
