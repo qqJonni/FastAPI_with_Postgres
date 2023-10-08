4240cb655e16fa22ae0e1ec40b54dbe863906ed8
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-08 20:25:35 +0500

    publish text to Telegram channel

--- Desktop/hlam/space_photos/telegram_bot.py
+++ Desktop/hlam/space_photos/telegram_bot.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 bot = telegram.Bot(os.environ.get('TELEGRAM_TOKEN'))
-print(bot.get_me())
+
+bot.send_message(chat_id='1500316931', text="I'm sorry Dave I'm afraid I can't do that.")
 
